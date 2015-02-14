f86adddeec21f1b7fe8a359339d56b4ec38d2e8b
marriola <azathoth500@gmail.com>
2015-02-13 23:23:25 -0500

    Added builtin list functions.

---
+++ builtin/list.d
@@ -0,0 +1,49 @@
+module builtin.list;
+
+import functions;
+import list;
+import token;
+
+Token builtinQuote (string name, ReferenceToken args) {
+    if (!hasMore(args)) {
+        throw new NotEnoughArgumentsException(name);
+    }
+
+    return getFirst(args);
+}
+
+Token builtinCons (string name, ReferenceToken args) {
+    if (listLength(args) < 2) {
+        throw new NotEnoughArgumentsException(name);
+    }
+
+    Token car = getFirst(args);
+    Token cdr = getFirst(getRest(args));
+    return Token.makeReference(car, cdr);
+}
+
+Token builtinCar (string name, ReferenceToken args) {
+    if (!hasMore(args)) {
+        throw new NotEnoughArgumentsException(name);
+    }
+
+    return getFirst(getFirst(args));
+}
+
+Token builtinCdr (string name, ReferenceToken args) {
+    if (!hasMore(args)) {
+        throw new NotEnoughArgumentsException(name);
+    }
+
+    return getRest(getFirst(args));
+}
+
+BuiltinFunction[string] addBuiltins (BuiltinFunction[string] builtinTable) {
+    builtinTable["QUOTE"] = &builtinQuote;
+    builtinTable["CONS"] = &builtinCons;
+    builtinTable["CAR"] = &builtinCar;
+    builtinTable["FIRST"] = &builtinCar;
+    builtinTable["CDR"] = &builtinCdr;
+    builtinTable["REST"] = &builtinCdr;
+    return builtinTable;
+}
