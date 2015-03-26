4a48f0c688d8ef8ad351aeab92f49d4bdcda9249
marriola <azathoth500@gmail.com>
2015-03-25 22:19:52 -0400

    Fibonacci example caches computed fibonacci numbers.

--- example/fib.lisp
+++ example/fib.lisp
@@ -1,1 +1,16 @@
-(defun fib (n) (if (<= n 2) 1 (+ (fib (- n 2)) (fib (- n 1)))))
+(setq fibcache (make-array 100))
+
+(defun fib (n)
+    (if (null (elt fibcache n))
+        (setf (elt fibcache n)
+            (if (<= n 2)
+                1
+                (+
+                    (fib (- n 2))
+                    (fib (- n 1))
+                )
+            )
+        )
+        (elt fibcache n)
+    )
+)
