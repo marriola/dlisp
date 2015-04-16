bd90c58170dc6e5c16bb2387739676a6ae9c747b
marriola <azathoth500@gmail.com>
2015-04-15 21:44:34 -0400

    Fixed infinite loop in case start > stop.

--- example/range.lisp
+++ example/range.lisp
@@ -1,6 +1,6 @@
 (defun range (start stop &optional out)
     "Returns a list containing numbers in the interval (start, stop]"
-    (if (= start stop)
+    (if (<= stop start)
         out
         (append
             (list start)
